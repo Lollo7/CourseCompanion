49bfdee8636c4a7cd1f6b3039b5d198b30f7be23
Russell-Day <russday@umich.edu>
2024-11-17 12:01:27 -0500

    hello

--- streamlit.py
+++ streamlit.py
@@ -2,7 +2,8 @@ import streamlit as st
 import streamlit.components.v1 as components
 import tools as tl
 
-
+calendar_html = """
+<iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FNew_York&showPrint=0&mode=AGENDA&showTabs=0&showTitle=0&showNav=0&src=cnVzc2VsbGRheTQyMkBnbWFpbC5jb20&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=NGE0OWViMGJlMDgzZDI2ZjkwMTg0YWMyMDA4YzM3N2I2ZjFlNmNjOTM0ODJiNTM0YTExNDAzZjNiODgyZTMwMkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%2333B679&color=%23EF6C00" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>"""
 
 def configurepage() -> None:
     st.set_page_config(page_title="Google Calendar API", page_icon=":calendar:", layout="centered", initial_sidebar_state="expanded")  
